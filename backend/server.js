23178a3fcb85db40124988202a20cbd9a855ee7c
balu <balakrishnan3010rgmail.com>
2026-06-17 02:08:29 +0530

    error solve

--- backend/server.js
+++ backend/server.js
@@ -139,7 +139,9 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
